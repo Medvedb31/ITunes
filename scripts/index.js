770d818448fc2500a1c339f0c4068d21bed1957b
Dmitry Cheplyanski <cheplyanskii2011@mail.ru>
2020-12-21 12:32:40 +0300

    init: copy my trainig project

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+'use strict'
+import { radioPlayerInit } from './radioPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+
+const deactivationPlayer = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+}
+
+playerBtn.forEach((btn,i) => btn.addEventListener('click', () => {
+    deactivationPlayer();
+    btn.classList.add('active');
+      playerBlock[i].classList.add('active');
+}));
+
+console.log(playerBlock);
+console.log(playerBtn);
+
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
---
+++ scripts/radioPlayer.js
@@ -0,0 +1,61 @@
+export const radioPlayerInit = () => {
+    
+
+const radio = document.querySelector('.radio');
+const radioNavigation = document.querySelector('.radio-navigation');
+const radioCover__img = document.querySelector('.radio-cover__img');
+const radioItem = document.querySelectorAll('.radio-item');
+const radioHeader = document.querySelector('.radio-header__big');
+const radioStop = document.querySelector('.radio-stop');
+
+const audio = new Audio();
+audio.type = 'audio/aac';
+radioStop.disabled = true;
+
+const changeIconPlay = () => {
+    if (audio.paused) {
+        radio.classList.remove('play');
+        radioStop.classList.add('fa-play');
+        radioStop.classList.remove('fa-stop');
+    } else {
+        radio.classList.add('play');
+        radioStop.classList.add('fa-stop');
+        radioStop.classList.remove('fa-play');
+    }
+};
+
+const selectItem = elem => {
+    radioItem.forEach(item => item.classList.remove('select'));
+    elem.classList.add('select');
+}
+radioNavigation.addEventListener('change' , event  => {
+    const target = event.target;
+    const parrent = target.closest('.radio-item');
+    
+    selectItem(parrent);
+    const title =parrent.querySelector('.radio-name').textContent;
+    radioHeader.textContent = title;
+    
+    const img = parrent.querySelector('.radio-img').src;
+    radioCover__img.src = img;
+
+    audio.src = target.dataset.radioStantion;
+    radioStop.disabled = false;
+    audio.play();
+    changeIconPlay();
+});
+
+radioStop.addEventListener('click' , () => {
+    if (audio.paused) {
+        audio.play();
+        
+    } else {
+        audio.pause();
+    };
+
+    changeIconPlay();
+});
+
+};
+
+
